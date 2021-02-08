6dcaba20405ed84b5aecbe56d2c29ef0b63353d0
Maureen Barasa <47135007+MaureenBarasa@users.noreply.github.com>
2021-02-09 00:31:18 +0300

    Add files via upload
    added terraform files

---
+++ XR3-Engine-Terraform/vars.tf
@@ -0,0 +1,15 @@
+variable "AWS_ACCESS_KEY" {
+  default = "AKIARQM6EGKHPL5LR47D"
+}
+
+variable "AWS_SECRET_KEY" {
+  default = "HPKPyVToA63Yq/WJLBUe4Lv3EIbizGoFHkmXEBpI"
+}
+
+variable "AWS_REGION" {
+  default = "us-west-1"
+}
+
+variable "key_name" {
+  default = "Gaming"
+}
